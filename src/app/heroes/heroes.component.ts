80d43bb9a9453edff62261f576d76ff0364c041b
Dominic Dopico <serafin.dopico@gmail.com>
2020-09-14 13:13:12 -0700

    details section update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,13 +12,14 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
