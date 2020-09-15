bc3ac5fed4a2cf87141f61e8aec828511ee8120d
Dominic Dopico <serafin.dopico@gmail.com>
2020-09-14 18:40:43 -0700

    add getHero() to HeroService, add getHero() to hero-detail component logic

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,12 +11,28 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
+  // Class props
   @Input() hero: Hero;
 
-  constructor() { }
+  // Constructor - initiates route, location, and heroService
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
+  // Lifecycle hook for component build onInit
   ngOnInit(): void {
+    this.getHero();
+  }
+
+
+  // Component methods
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,11 +12,22 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
+  // Constructor - instantiates messageService
   constructor(private messageService: MessageService) { }
 
+
+  // Component methods
+  // Returns observable of heroes array
   getHeroes(): Observable<Hero[]> {
     // TODO: send message _after_ fetching heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  // Takes id: number as a param, returns observable of specific hero by id in array
+  getHero(id: number): Observable<Hero> {
+    // TODO: send message _after_ fetching hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
